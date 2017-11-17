41e36768206a40d97ac7869c8b415a6c056e2a6f
aespinet <aelluvetaa@gmail.com>
2017-11-17 10:46:02 -0500

    production env Part 3

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,22 @@
 import * as firebase from 'firebase';
 
+// const config = {
+//   apiKey: process.env.FIREBASE_API_KEY || "AIzaSyB-b-XAhXumHtAvEpLFPSgbs-Ysg9ggD-0",
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN || "expensify-63d56.firebaseapp.com",
+//   databaseURL: process.env.FIREBASE_DATABASE_URL || "https://expensify-63d56.firebaseio.com",
+//   projectId: process.env.FIREBASE_PROJECT_ID || "expensify-63d56",
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "expensify-63d56.appspot.com",
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "804755885988"
+// };
+
+
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY || "AIzaSyB-b-XAhXumHtAvEpLFPSgbs-Ysg9ggD-0",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "expensify-63d56.firebaseapp.com",
-  databaseURL: process.env.FIREBASE_DATABASE_URL || "https://expensify-63d56.firebaseio.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "expensify-63d56",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "expensify-63d56.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "804755885988"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
